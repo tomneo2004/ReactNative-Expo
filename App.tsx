fd959c32ccf3816918832ff5fc9f0835f8c69347
Nelson <tomneo2004@gmail.com>
2020-11-15 06:46:21 +0800

    Navigator route

--- App.tsx
+++ App.tsx
@@ -1,107 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
-import Button from './components/Button';
-// import logo from './assets/logo.png';
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing'; 
-import uploadToAnonymousFilesAsync from 'anonymous-files'; 
-import Logo from './components/Logo';
-import Constants from 'expo-constants';
-// import * as SplashScreen from 'expo-splash-screen';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// SplashScreen.preventAutoHideAsync()
-// .then(()=>setTimeout(async ()=> await SplashScreen.hideAsync(), 5000))
-// .catch(err=>console.log(err));
-
-
-export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState<{[key:string]:string} | null>(null);
-
-  let openImagePickerAsync = async ()=>{
-    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-    if(permissionResult.granted === false){
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if(pickerResult.cancelled === true) return;
-
-    if(Platform.OS === 'web'){
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({localUri: pickerResult.uri, remoteUri});
-    }
-    else{
-      setSelectedImage({localUri: pickerResult.uri, remoteUri:null});
-    }
-  }
-
-  let openShareDialogAsync = async ()=>{
-    if(!await Sharing.isAvailableAsync()){
-      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  }
-
-  const clearImage = ()=>{
-    setSelectedImage(null);
-  }
-
-  if(selectedImage !== null){
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-        <Text style={styles.instruction}>
-        To share a photo from your phone with a friend, just press the button below!
-        </Text>
-        <Button title='Pick a photo' onPress={openImagePickerAsync} />
-        <Button title='Share this photo' onPress={openShareDialogAsync} />
-        <Button title='Clear the image' onPress={clearImage} />
-        <StatusBar style="auto" />
-      </View>
-    )
-  }
-
+function HomeScreen(){
   return (
-    <View style={styles.container}>
-      {/* <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} /> */}
-      <Logo title={Constants.manifest.extra.logoTitle}/>
-      <Text style={styles.instruction}>
-        To share a photo from your phone with a friend, just press the button below!
-      </Text>
-      <Button title='Pick a photo' onPress={openImagePickerAsync} />
-      <StatusBar style="auto" />
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    width: 305, 
-    height: 159,
-    marginBottom: 10,
-  },
-  instruction:{
-    color:'#888',
-    fontSize:18,
-    marginHorizontal: 15,
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain'
-  }
-});
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{title: 'View'}}>
+        <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Overview' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
---
+++ views/imageShare.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
+import Button from '../components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+import uploadToAnonymousFilesAsync from 'anonymous-files'; 
+import Logo from '../components/Logo';
+import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+
+const ImageShare = () => {
+    const [selectedImage, setSelectedImage] = React.useState<{[key:string]:string} | null>(null);
+
+    let openImagePickerAsync = async ()=>{
+      let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+      if(permissionResult.granted === false){
+        alert("Permission to access camera roll is required!");
+        return;
+      }
+  
+      let pickerResult = await ImagePicker.launchImageLibraryAsync();
+      
+      if(pickerResult.cancelled === true) return;
+  
+      if(Platform.OS === 'web'){
+        let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+        setSelectedImage({localUri: pickerResult.uri, remoteUri});
+      }
+      else{
+        setSelectedImage({localUri: pickerResult.uri, remoteUri:null});
+      }
+    }
+  
+    let openShareDialogAsync = async ()=>{
+      if(!await Sharing.isAvailableAsync()){
+        alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+        return;
+      }
+  
+      await Sharing.shareAsync(selectedImage.localUri);
+    }
+  
+    const clearImage = ()=>{
+      setSelectedImage(null);
+    }
+  
+    if(selectedImage !== null){
+      return (
+        <View style={styles.container}>
+          <Image
+            source={{ uri: selectedImage.localUri }}
+            style={styles.thumbnail}
+          />
+          <Text style={styles.instruction}>
+          To share a photo from your phone with a friend, just press the button below!
+          </Text>
+          <Button title='Pick a photo' onPress={openImagePickerAsync} />
+          <Button title='Share this photo' onPress={openShareDialogAsync} />
+          <Button title='Clear the image' onPress={clearImage} />
+          <StatusBar style="auto" />
+        </View>
+      )
+    }
+  
+    return (
+      <View style={styles.container}>
+        {/* <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} /> */}
+        <Logo title={Constants.manifest.extra.logoTitle}/>
+        <Text style={styles.instruction}>
+          To share a photo from your phone with a friend, just press the button below!
+        </Text>
+        <Button title='Pick a photo' onPress={openImagePickerAsync} />
+        <StatusBar style="auto" />
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{
+      width: 305, 
+      height: 159,
+      marginBottom: 10,
+    },
+    instruction:{
+      color:'#888',
+      fontSize:18,
+      marginHorizontal: 15,
+    },
+    thumbnail: {
+      width: 300,
+      height: 300,
+      resizeMode: 'contain'
+    }
+  });
+
+export default ImageShare;
