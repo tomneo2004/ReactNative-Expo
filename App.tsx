0aecd85adc17c931a5b012908163a0eb8fd7ce60
Nelson <tomneo2004@gmail.com>
2020-11-16 06:43:20 +0800

    Navigate route

--- App.tsx
+++ App.tsx
@@ -1,25 +1,45 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackScreenProps } from '@react-navigation/stack';
+import Button from './components/Button';
 
-function HomeScreen(){
+type TStackParamList = {
+  [key:string]:any
+}
+
+type IHomeProps = StackScreenProps<TStackParamList, 'Home'>;
+
+function HomeScreen(props:IHomeProps){
+  const {
+    navigation
+  } = props;
+
   return (
     <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
       <Text>Home Screen</Text>
+      <Button title='Go To Detail' onPress={()=>navigation.navigate('Details')} />
     </View>
   )
 }
 
-function DetailsScreen() {
+type IDetailsProps = StackScreenProps<TStackParamList, 'Home'>;
+
+function DetailsScreen(props:IDetailsProps) {
+  const {
+    navigation
+  } = props;
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button title='Go To Detail' onPress={()=>navigation.push('Details')} />
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+
+const Stack = createStackNavigator<TStackParamList>();
 
 export default function App() {
   return (
