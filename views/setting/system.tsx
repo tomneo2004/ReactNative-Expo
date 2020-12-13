311776a657074043d62be13a2fd763fda5e14cfa
Nelson <tomneo2004@gmail.com>
2020-12-14 06:43:14 +0800

    Group Button

--- views/setting/system.tsx
+++ views/setting/system.tsx
@@ -2,6 +2,7 @@ import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { ButtonGroup } from 'react-native-elements';
 import FocusAwareStatusBar from '../../components/FocusAwareStatusBar';
 import { TTabeParamList } from './setting';
 
@@ -12,13 +13,22 @@ export interface ISystemParams {
 interface ISystemProps extends BottomTabScreenProps<TTabeParamList, 'System'>{}
 interface ISystemRouteProps extends RouteProp<TTabeParamList, 'System'>{};
 
+const buttons = ['Hello', 'World', 'Buttons'];
+
 const System = (props:ISystemProps) => {
     const route = useRoute<ISystemRouteProps>();
+    const [index, setIndex] = React.useState<number>(1);
     return (
         <SafeAreaView style={{flex:1, justifyContent:'center', alignItems:'center'}}>
             <FocusAwareStatusBar style="light" backgroundColor="#6a51ae" />
             <Text>This is system screen</Text>
             <Text>{route.params.ownerName}</Text>
+            <ButtonGroup
+            onPress={(selectedIndex)=>setIndex(selectedIndex)}
+            selectedIndex={index}
+            buttons={buttons}
+            // containerStyle={{height: 100}}
+            />
         </SafeAreaView>
     );
 };
