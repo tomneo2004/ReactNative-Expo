14f361c13eadc2665a5c21b2d2e7a96b62cc6452
Nelson <tomneo2004@gmail.com>
2020-12-14 06:15:34 +0800

    Bottom Sheet

--- views/setting/profile.tsx
+++ views/setting/profile.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { View, Text, SafeAreaView, StatusBar } from 'react-native';
 import FocusAwareStatusBar from '../../components/FocusAwareStatusBar';
 import {TTabeParamList} from './setting';
+import CButton from '../../components/Button';
+import { BottomSheet, ListItem } from 'react-native-elements';
 
 export interface IProfileParams {
     username: string;
@@ -15,11 +17,39 @@ const Profile = (props:IProfileProps) => {
         route,
     } = props;
 
+    const [showMenu, setShowMenu] = React.useState<boolean>(false);
+
+    const list = [
+        { title: 'List Item 1' },
+        { title: 'List Item 2' },
+        {
+          title: 'Cancel',
+          containerStyle: { backgroundColor: 'red' },
+          titleStyle: { color: 'white' },
+          onPress: () => setShowMenu(false),
+        },
+      ];
+
     return (
         <SafeAreaView style={{flex:1, justifyContent:'center', alignItems:'center'}}>
             <FocusAwareStatusBar style="dark" backgroundColor="#ecf0f1" />
             <Text>This is profile screen</Text>
             <Text>{route.params.username}</Text>
+            <CButton 
+            title='Bottom Menu'
+            onPress={()=>setShowMenu(true)}
+            />
+            <BottomSheet isVisible={showMenu} modalProps={{
+                animationType:'slide',
+            }}>
+            {list.map((l, i) => (
+                <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
+                    <ListItem.Content>
+                    <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+            ))}
+            </BottomSheet>
         </SafeAreaView>
     );
 };
