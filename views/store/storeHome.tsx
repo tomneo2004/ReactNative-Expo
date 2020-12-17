d231163db4758096267727adbad47ff492058bd3
Nelson <tomneo2004@gmail.com>
2020-12-18 06:29:26 +0800

    PricingCard

--- views/store/storeHome.tsx
+++ views/store/storeHome.tsx
@@ -7,6 +7,7 @@ import FocusAwareStatusBar from '../../components/FocusAwareStatusBar';
 import CButton from '../../components/Button';
 import i18n from '../../i18n';
 import { useTranslation } from 'react-i18next';
+import { PricingCard } from 'react-native-elements';
 
 export interface IStoreHomeParams {}
 
@@ -25,6 +26,15 @@ const StoreHome = (props:IStoreHomeProps) => {
             <CButton title='EN' onPress={()=>i18n.changeLanguage('en')} />
             <CButton title='FR' onPress={()=>i18n.changeLanguage('fr')} />
             <Text>{t('foo')}</Text>
+            <PricingCard 
+            color="#4f9deb"
+            title='Membership'
+            price="$40 / Month"
+            info={['1 User', 'Basic Support', 'All Core Features']}
+            button={{ title: 'GET STARTED', icon: 'book' }}
+            pricingStyle={{fontSize:20}}
+            containerStyle={{borderRadius:5}}
+            />
         </SafeAreaView>
         
     );
