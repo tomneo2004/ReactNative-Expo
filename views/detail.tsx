65adddac163945f85ee6d24a12b969ff831dd2a0
Nelson <tomneo2004@gmail.com>
2020-12-22 06:22:45 +0800

    Tile

--- views/detail.tsx
+++ views/detail.tsx
@@ -3,7 +3,7 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { View, Text, SafeAreaView, StatusBar } from 'react-native';
 import Button from '../components/Button';
 import {TStackParamList} from '../navigation/navigation';
-import { Rating } from 'react-native-elements';
+import { Rating, Tile } from 'react-native-elements';
 
 export interface IDetailParams {
     itemId:number;
@@ -20,6 +20,15 @@ export default function DetailsScreen(props:IDetailsProps) {
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'darkgrey' }}>
       <StatusBar barStyle="dark-content" backgroundColor="#ecf0f1" />
+      <Tile
+      imageSrc={{uri:'https://images.unsplash.com/photo-1521317925431-c2256dd4fe2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80'}}
+      icon={{ name: 'play-circle', type: 'font-awesome', size:44, color:'lightgrey' }}
+      title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolore exercitationem"
+      caption="Some Caption Text"
+      featured
+      width={300}
+      overlayContainerStyle={{backgroundColor:'rgba(23,23,23,0.5)'}}
+      />
       <Text>Details Screen</Text>
       <Text>Item ID: {route.params.itemId}</Text>
       <Button title='Go To Detail' onPress={()=>navigation.push('Details')} />
